3f743c2a2480bb97a66ca3799483a7d5a4a5a619
Savio-Castro <saviocastro2511@gmail.com>
2023-05-11 23:33:48 -0300

    Exercicio2

---
+++ Exercicio2.c
@@ -0,0 +1,39 @@
+//Faça um programa que leia um número inteiro positivo e verifique se ele é um número primo.
+#include <stdio.h>
+int main (){
+     int x;
+
+ 
+
+printf ("Digite um numero qualquer:");
+scanf ("%d",&x);
+
+ 
+
+if (x/x!=0){
+printf ("seu numero e primo");
+}
+else {
+printf ("nao e primo");
+}
+return (0);
+}
+
+
+ 
+
+printf("seu numero e = %d \n", numero);
+}
+else {
+    printf ("sinto muito, digite outro numero\n");
+
+ 
+
+}
+}
+while (numero!=0||numero<0);
+
+ 
+
+return (0);
+}
