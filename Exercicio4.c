7d92fde464fe66c7465b7cf4bb7fbd8ae0b3d0bf
Savio-Castro <saviocastro2511@gmail.com>
2023-05-11 23:40:02 -0300

    Exercio4

--- Exercicio4.c
+++ Exercicio4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (){
 int num=0, i;
-printf("DIGITE UM NUMERO:");
+printf("DIGITE UM NUMERO INTEIRO:");
 scanf ("%d", &num);
 for (i = 1; i <= 10; ++i) {
 printf("%d x %d = %d\n", num, i, num*i);
